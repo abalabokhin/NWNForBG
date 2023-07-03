ce1fa92d775ad207d91073885c696e6dfe900a88
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2023-07-03 06:36:49 -0400

    Added more missing resources

--- bash_scripts/cp_rs.sh
+++ bash_scripts/cp_rs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-filename='missing_cre.txt'
+filename='missing_bcs.txt'
 echo Start
 while read p; do
-    cp /home/paladin/Games/exported_cre/${p} ../NWNForBG/cre
+    cp /home/paladin/Games/exported_script/${p} ../NWNForBG/scripts
 #    mmv "${sa[1]}*" "${sa[0]}#1"
 #    rename ${sa[1]} ${sa[0]} *
 #    echo ${sa[0]} ${sa[1]}
