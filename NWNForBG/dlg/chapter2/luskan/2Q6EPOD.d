e0d33ce8bf4aa8c7d85b415197cb981f36fe8f38
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2023-07-04 16:28:12 -0400

    Fix d wrong and change IDS

--- NWNForBG/dlg/chapter2/luskan/2Q6EPOD.d
+++ NWNForBG/dlg/chapter2/luskan/2Q6EPOD.d
@@ -7,14 +7,14 @@
 BEGIN ~2Q6EPOD~
 
 IF WEIGHT #0 /* Triggers after states #: 9 even though they appear after this state */
-~   GlobaLT("GolemCreated","MYAREA",2)~ THEN BEGIN 0 // from:
+~   GlobalLT("GolemCreated","MYAREA",2)~ THEN BEGIN 0 // from:
   SAY ~Эта камера сделана из мрамора, в ее центре установлены сложные кристаллы, а с краю имеются два углубления. Ее прямо-таки распирает от скопившейся внутри магической энергии.~
-  IF ~  Globa("GolemCreated","MYAREA",0)~ THEN REPLY ~Рассмотреть углубления.~ GOTO 1
-  IF ~  Globa("GolemCreated","MYAREA",1)~ THEN REPLY ~Рассмотреть углубления.~ GOTO 2
-  IF ~  PartyHasItem("Wandnw5")Globa("GolemCreated","MYAREA",0)~ THEN REPLY ~Вставить жезл управления големами в углубление.~ GOTO 3
-  IF ~  PartyHasItem("Wandnw5")Globa("GolemCreated","MYAREA",1)~ THEN REPLY ~Вставить жезл управления големами в углубление.~ GOTO 4
-  IF ~  PartyHasItem("Wandnw6")Globa("GolemCreated","MYAREA",0)~ THEN REPLY ~Вставить жезл производства големов в углубление.~ GOTO 5
-  IF ~  PartyHasItem("Wandnw6")Globa("GolemCreated","MYAREA",1)~ THEN REPLY ~Вставить жезл производства големов в углубление.~ GOTO 6
+  IF ~  Global("GolemCreated","MYAREA",0)~ THEN REPLY ~Рассмотреть углубления.~ GOTO 1
+  IF ~  Global("GolemCreated","MYAREA",1)~ THEN REPLY ~Рассмотреть углубления.~ GOTO 2
+  IF ~  PartyHasItem("Wandnw5")Global("GolemCreated","MYAREA",0)~ THEN REPLY ~Вставить жезл управления големами в углубление.~ GOTO 3
+  IF ~  PartyHasItem("Wandnw5")Global("GolemCreated","MYAREA",1)~ THEN REPLY ~Вставить жезл управления големами в углубление.~ GOTO 4
+  IF ~  PartyHasItem("Wandnw6")Global("GolemCreated","MYAREA",0)~ THEN REPLY ~Вставить жезл производства големов в углубление.~ GOTO 5
+  IF ~  PartyHasItem("Wandnw6")Global("GolemCreated","MYAREA",1)~ THEN REPLY ~Вставить жезл производства големов в углубление.~ GOTO 6
   IF ~~ THEN REPLY ~Разрушить камеру.~ GOTO 7
   IF ~~ THEN REPLY ~Оставить камеру в покое.~ GOTO 8
 END
@@ -37,32 +37,32 @@ END
 
 IF ~~ THEN BEGIN 3 // from: 1.3
   SAY ~Жезл легко скользит на место и останавливается с легким щелчком. Она начинает тускло светиться, и камера жужжит... Хотя пока ничего не происходит.~
-  IF ~~ THEN DO ~SetGloba("GolemCreated","MYAREA",1)TakePartyItem("Wandnw5")DestroyItem("Wandnw5")~ EXIT
+  IF ~~ THEN DO ~SetGlobal("GolemCreated","MYAREA",1)TakePartyItem("Wandnw5")DestroyItem("Wandnw5")~ EXIT
 END
 
 IF ~~ THEN BEGIN 4 // from: 1.4
   SAY ~Второй жезл входит на место рядом с первым и останавливается с легким щелчком. Оба жезла начинают вдруг ярко светиться, и камера оживает...~
-  IF ~~ THEN DO ~SetGloba("GolemCreated","MYAREA",2)TakePartyItem("Wandnw5")DestroyItem("Wandnw5")~ EXIT
+  IF ~~ THEN DO ~SetGlobal("GolemCreated","MYAREA",2)TakePartyItem("Wandnw5")DestroyItem("Wandnw5")~ EXIT
 END
 
 IF ~~ THEN BEGIN 5 // from: 1.5
   SAY ~Жезл легко скользит на место и останавливается с легким щелчком. Она начинает тускло светиться, и камера жужжит... Хотя пока ничего не происходит.~
-  IF ~~ THEN DO ~SetGloba("GolemCreated","MYAREA",1)TakePartyItem("Wandnw6")DestroyItem("Wandnw6")~ EXIT
+  IF ~~ THEN DO ~SetGlobal("GolemCreated","MYAREA",1)TakePartyItem("Wandnw6")DestroyItem("Wandnw6")~ EXIT
 END
 
 IF ~~ THEN BEGIN 6 // from: 1.6
   SAY ~Второй жезл входит на место рядом с первым и останавливается с легким щелчком. Оба жезла начинают вдруг ярко светиться, и камера оживает...~
-  IF ~~ THEN DO ~SetGloba("GolemCreated","MYAREA",2)TakePartyItem("Wandnw6")DestroyItem("Wandnw6")~ EXIT
+  IF ~~ THEN DO ~SetGlobal("GolemCreated","MYAREA",2)TakePartyItem("Wandnw6")DestroyItem("Wandnw6")~ EXIT
 END
 
 IF ~~ THEN BEGIN 7 // from: 1.7
   SAY ~Вы пытаетесь ударить по камере, но ваш удар отражается от невидимого барьера, окружающего поверхность.~
-  IF ~  Globa("GolemCreated","MYAREA",0)~ THEN REPLY ~Рассмотреть углубления.~ GOTO 1
-  IF ~  Globa("GolemCreated","MYAREA",1)~ THEN REPLY ~Рассмотреть углубления.~ GOTO 2
-  IF ~  PartyHasItem("Wandnw5")Globa("GolemCreated","MYAREA",0)~ THEN REPLY ~Вставить жезл управления големами в углубление.~ GOTO 3
-  IF ~  PartyHasItem("Wandnw5")Globa("GolemCreated","MYAREA",1)~ THEN REPLY ~Вставить жезл управления големами в углубление.~ GOTO 4
-  IF ~  PartyHasItem("Wandnw6")Globa("GolemCreated","MYAREA",0)~ THEN REPLY ~Вставить жезл производства големов в углубление.~ GOTO 5
-  IF ~  PartyHasItem("Wandnw6")Globa("GolemCreated","MYAREA",1)~ THEN REPLY ~Вставить жезл производства големов в углубление.~ GOTO 6
+  IF ~  Global("GolemCreated","MYAREA",0)~ THEN REPLY ~Рассмотреть углубления.~ GOTO 1
+  IF ~  Global("GolemCreated","MYAREA",1)~ THEN REPLY ~Рассмотреть углубления.~ GOTO 2
+  IF ~  PartyHasItem("Wandnw5")Global("GolemCreated","MYAREA",0)~ THEN REPLY ~Вставить жезл управления големами в углубление.~ GOTO 3
+  IF ~  PartyHasItem("Wandnw5")Global("GolemCreated","MYAREA",1)~ THEN REPLY ~Вставить жезл управления големами в углубление.~ GOTO 4
+  IF ~  PartyHasItem("Wandnw6")Global("GolemCreated","MYAREA",0)~ THEN REPLY ~Вставить жезл производства големов в углубление.~ GOTO 5
+  IF ~  PartyHasItem("Wandnw6")Global("GolemCreated","MYAREA",1)~ THEN REPLY ~Вставить жезл производства големов в углубление.~ GOTO 6
   IF ~~ THEN REPLY ~Оставить камеру в покое.~ GOTO 8
 END
 
@@ -75,7 +75,7 @@ END
 // -------------------------------------
 
 IF WEIGHT #1 /* Triggers after states #: 9 even though they appear after this state */
-~   GlobaGT("GolemCreated","MYAREA",1)~ THEN BEGIN 9 // from:
+~   GlobalGT("GolemCreated","MYAREA",1)~ THEN BEGIN 9 // from:
   SAY ~Камера испускает легкие клубы дыма. Обе палочки в ее углублениях почернели и обгорели. Какая бы магия ни питала эту камеру, похоже, вся она истощилась.~
   IF ~~ THEN EXIT
 END
