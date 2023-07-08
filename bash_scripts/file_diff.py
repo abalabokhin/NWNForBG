a624958c05c0e02f0886bd5e5ea1fff2c67643f7
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2023-07-08 16:30:53 -0400

    update missing res file

--- bash_scripts/file_diff.py
+++ bash_scripts/file_diff.py
@@ -1,5 +1,5 @@
 f1 = open('missing_res.txt').readlines()
-f2 = open('orig_missing_res.txt').readlines()
+f2 = open('missing_res_orig.txt').readlines()
 f1 = [i.replace('\n', '') for i in f1]
 f2 = [i.replace('\n', '') for i in f2]
 l = [a for a in f1 if all(b not in a for b in f2)]
