52b5dac492e0888b985acc944e40b654b8cf1eae
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2023-07-08 18:01:16 -0400

    Fix script to find diff

--- bash_scripts/file_diff.py
+++ bash_scripts/file_diff.py
@@ -1,6 +1,20 @@
-f1 = open('missing_res.txt').readlines()
-f2 = open('missing_res_orig.txt').readlines()
-f1 = [i.replace('\n', '') for i in f1]
-f2 = [i.replace('\n', '') for i in f2]
-l = [a for a in f1 if all(b not in a for b in f2)]
-print('\n'.join(l))
+#!/usr/bin/env python3
+
+import sys
+
+def diff_and_save(orig_name):
+    orig_end = "_orig.txt"
+    if not orig_name.endswith(orig_end):
+        return
+    basename = orig_name[0:-len(orig_end)]
+    filename = basename + ".txt"
+    f1 = open(filename).readlines()
+    f2 = open(orig_name).readlines()
+    f1 = [i.replace('\n', '') for i in f1]
+    f2 = [i.replace('\n', '') for i in f2]
+    l = [a for a in f1 if all(b not in a for b in f2)]
+    print('\n'.join(l), file=open(basename + "_diff.txt", 'w'))
+
+
+diff_and_save('missing_res_orig.txt')
+
