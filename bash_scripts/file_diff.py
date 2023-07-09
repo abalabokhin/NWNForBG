ebad54aa359e4227423f39ac8a4db2cc6f0358a2
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2023-07-09 02:37:23 -0400

    update missing resources

--- bash_scripts/file_diff.py
+++ bash_scripts/file_diff.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+import re
+import shutil
+import os
 
 def diff_and_save(orig_name):
     orig_end = "_orig.txt"
@@ -18,9 +20,61 @@ def diff_and_save(orig_name):
     print('\n'.join(uniq_lines), file=open(basename + "_diff.txt", 'w'))
 
 
-def parse1(filename):
+extensions = {"itm": "itm",
+              "spl": "spl",
+              "vef": "vvc",
+              "wav": "wav",
+              "bcs": "baf",
+              "cre": "cre",
+              "eff": "eff",
+              "bmp": "bmp",
+              "bam": "bam"}
+
+
+def parse1(filename, result):
     f1 = open(filename).readlines()
-    print(f1)
+    for l in f1:
+        for ext in extensions:
+            m = re.search("=([^=]*.{})".format(ext.upper()), l)
+            if m:
+                res_name = m.group(1).replace(".{}".format(ext.upper()), ".{}".format(extensions[ext].upper()))
+                if ext not in result:
+                    result[ext] = set()
+                result[ext].add(res_name)
+
+
+def parse2(filename, result):
+    f1 = open(filename).readlines()
+    for l in f1:
+        for ext in extensions:
+            m = re.search("\\[{}\\] - (.*)".format(ext.upper()), l)
+            if m:
+                res_name = m.group(1).upper() + "." + format(extensions[ext].upper())
+                if ext not in result:
+                    result[ext] = set()
+                result[ext].add(res_name)
+
+
+def parse3(filename, result):
+    f1 = open(filename).readlines()
+    for l in f1:
+        m = re.search("Item: (.*\\.ITM)", l)
+        if m:
+            res_name = m.group(1).upper()
+            if 'itm' not in result:
+                result['itm'] = set()
+            result['itm'].add(res_name)
+
+
+def parse4(filename, result):
+    f1 = open(filename).readlines()
+    for l in f1:
+        m = re.search("Sound: ([^,]*),", l)
+        if m:
+            res_name = m.group(1).upper() + ".WAV"
+            if 'wav' not in result:
+                result['wav'] = set()
+            result['wav'].add(res_name)
 
 
 diff_and_save('missing_res_orig.txt')
@@ -32,4 +86,32 @@ diff_and_save('unknown_ids1_orig.txt')
 diff_and_save('unknown_ids2_orig.txt')
 
 result = {}
-parse1("missing_res_diff.txt")
+parse1("missing_res_diff.txt", result)
+parse2("dialog_check_diff.txt", result)
+parse3("items_not_in_inventory_diff.txt", result)
+parse4("missing_sound_refs_diff.txt", result)
+parse2("script_check_diff.txt", result)
+
+print(result)
+sources = {"itm": ["itm", "itm"],
+           "spl": ["spl", "spl"],
+           "vef": ["vvc", "vvc"],
+           "wav": ["wav", "audio"],
+           "bcs": ["script", "scripts"],
+           "cre": ["cre", "cre"],
+           "eff": ["eff", "eff"],
+           "bmp": ["bmp", "bmp"],
+           "bam": ["bam", "bam"]}
+
+copy_from = "/home/paladin/Games/exported_"
+copy_to = "/home/paladin/source/NWNForBG/NWNForBG/"
+for s in sources:
+    if s in result:
+        for res in result[s]:
+            cfrom = copy_from + sources[s][0] + "/" + res
+            if os.path.isfile(cfrom):
+            # print(copy_from + sources[s][0] + "/" + res, copy_to + sources[s][1])
+                shutil.copy(copy_from + sources[s][0] + "/" + res, copy_to + sources[s][1])
+
+
+
