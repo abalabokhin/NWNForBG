b496b19cce2f2359097537628b05f4da044d01c9
Andrey Balabokhin <balabokhin@correlatedsolutions.com>
2023-07-29 07:18:09 -0400

    J

--- NWNForBG/dlg/chapter1/peninsula/SEDOS.d
+++ NWNForBG/dlg/chapter1/peninsula/SEDOS.d
@@ -107,7 +107,7 @@ END
 
 IF ~~ THEN BEGIN 15 // from: 11.4
   SAY @33
-  IF ~~ THEN DO ~SetGlobal("SedosQuest","GLOBAL",2)EraseJournalEntry(@64)~ UNSOLVED_JOURNAL @34 EXIT
+  IF ~~ THEN DO ~SetGlobal("SedosQuest","GLOBAL",2)EraseJournalEntry(@93969)~ UNSOLVED_JOURNAL @94004 EXIT
 END
 
 IF ~~ THEN BEGIN 16 // from: 11.5
@@ -199,17 +199,17 @@ END
 
 IF ~~ THEN BEGIN 28 // from: 25.1
   SAY @59
-  IF ~~ THEN DO ~SetGlobal("SedosQuest","GLOBAL",4)GiveGoldForce(1000)AddexperienceParty(154000)EraseJournalEntry(@64)EraseJournalEntry(@34)~ SOLVED_JOURNAL @60 EXIT
+  IF ~~ THEN DO ~SetGlobal("SedosQuest","GLOBAL",4)GiveGoldForce(1000)AddexperienceParty(154000)EraseJournalEntry(@93969)EraseJournalEntry(@94004)~ SOLVED_JOURNAL @94059 EXIT
 END
 
 IF ~~ THEN BEGIN 29 // from: 25.2
   SAY @61
-  IF ~~ THEN DO ~SetGlobal("SedosQuest","GLOBAL",4)GiveGoldForce(1000)AddexperienceParty(154000)ReputationInc(-1)EraseJournalEntry(@64)EraseJournalEntry(@34)~ SOLVED_JOURNAL @60 EXIT
+  IF ~~ THEN DO ~SetGlobal("SedosQuest","GLOBAL",4)GiveGoldForce(1000)AddexperienceParty(154000)ReputationInc(-1)EraseJournalEntry(@93969)EraseJournalEntry(@94004)~ SOLVED_JOURNAL @94059 EXIT
 END
 
 IF ~~ THEN BEGIN 30 // from: 25.3
   SAY @62
-  IF ~~ THEN DO ~SetGlobal("SedosQuest","GLOBAL",4)AddexperienceParty(154000)ReputationInc(1)EraseJournalEntry(@64)EraseJournalEntry(@34)~ SOLVED_JOURNAL @60 EXIT
+  IF ~~ THEN DO ~SetGlobal("SedosQuest","GLOBAL",4)AddexperienceParty(154000)ReputationInc(1)EraseJournalEntry(@93969)EraseJournalEntry(@94004)~ SOLVED_JOURNAL @94059 EXIT
 END
 
 // ---------------------------------------------------------------------------------------
